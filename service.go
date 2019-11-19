5fce199786a8e678cbf567f84aed0b18f577e816
Siddhartha Basu <biosidd@gmail.com>
2019-11-19 11:46:47 -0600

    feat: add functions for creating grpc services

---
+++ service.go
@@ -0,0 +1,43 @@
+package aphgrpc
+
+import (
+	"context"
+
+	"github.com/fatih/structs"
+)
+
+// AssignFieldsToStructs copy fields value
+// between structure
+func AssignFieldsToStructs(from interface{}, to interface{}) {
+	toR := structs.New(to)
+	for _, f := range structs.New(from).Fields() {
+		if !f.IsZero() {
+			if nf, ok := toR.FieldOk(f.Name()); ok {
+				nf.Set(f.Value())
+			}
+		}
+	}
+}
+
+type ServiceOptions struct {
+	Topics   map[string]string
+	Resource string
+}
+
+type Option func(*ServiceOptions)
+
+func TopicsOption(t map[string]string) Option {
+	return func(so *ServiceOptions) {
+		so.Topics = t
+	}
+}
+
+type Service struct {
+	Resource string
+	Context  context.Context
+	Topics   map[string]string
+}
+
+func (s *Service) GetResourceName() string {
+	return s.Resource
+}
