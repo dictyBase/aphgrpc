226543cf46b7668f679b9783793762bd71125883
Siddhartha Basu <biosidd@gmail.com>
2019-11-19 14:22:34 -0600

    feat: add nats command line flags

---
+++ flag.go
@@ -0,0 +1,22 @@
+package aphgrpc
+
+import (
+	"github.com/urfave/cli"
+)
+
+// NatsFlag returns a cli.Flag slice for using in the command
+// line arguments
+func NatsFlag() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:   "nats-host",
+			EnvVar: "NATS_SERVICE_HOST",
+			Usage:  "nats messaging server host",
+		},
+		cli.StringFlag{
+			Name:   "nats-port",
+			EnvVar: "NATS_SERVICE_PORT",
+			Usage:  "nats messaging server port",
+		},
+	}
+}
